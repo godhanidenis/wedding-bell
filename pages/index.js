e8df2beb40ff9ce99c28eafe6be596b7b32b25f7
yadnesh <GDR2252>
2022-10-16 22:51:02 -0700

    design done product detail page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import ProductDetail from "./product/[id]";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <ProductDetail/>
     </>
   );
 }
